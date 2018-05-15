ac596df0d1056c135a18e059b3a4510e7201f3a0
Ratibor <klov29@mail.ru>
2018-05-15 14:01:00 +0700

    Закончил функционал

--- js/common.js
+++ js/common.js
@@ -1,13 +1,37 @@
 $(function () {
-    $('.header-page__input').click(function() {
+    $('.header-page__input').click(function(e) {
         var elem = $(this);
+        func(elem, '.header-page__menu-mob');
+    });
 
+    $('.header-page__active-link').click(function (e) {
+        e.preventDefault();
+        var elem = $(this);
+        func(elem, '.header-page__dropdown');
+    });
+
+    function func(elem, element) {
         elem.toggleClass('header-page__btn--active');
         if(elem.hasClass('header-page__btn--active')) {
-            $('.header-page__menu-mob').slideDown(500);
+            $(element).slideDown(500);
         } else {
-            $('.header-page__menu-mob').slideUp(500);
+            $(element).fadeOut(200);
         }
+    }
+
+    $(".login-js, .header-page__login, .header-page__login-link").on("click", function(e) {
+        e.preventDefault();
+        var currentModal = $(this).attr("href");
+
+        $(currentModal + ", .overlay").fadeIn(500);
+        $("body").addClass("open-modal");
+    });
+
+
+    $(".overlay, .btn").on("click", function(e) {
+        e.preventDefault();
+        $(".modal, .overlay").fadeOut(100);
+        $("body").removeClass("open-modal");
     });
 
 
